62f4e1c1706190b503bdbee706d5c389800f9729
Nicolas <nicolasperez95.a@gmail.com>
2020-10-29 05:53:51 -0300

    feat: added some test

---
+++ src/utils/is-url.ts
@@ -0,0 +1,5 @@
+const urlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+
+export const isUrl = (url: string): boolean => {
+  return urlRegex.test(url);
+}
